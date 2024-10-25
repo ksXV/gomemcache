55b7d501e6a940091664a0919553e3c4436eb657
ksXV <msgcipi@gmail.com>
2024-10-25 17:41:38 +0300

    fixed ignored error in memcached.go

--- memcache/memcache.go
+++ memcache/memcache.go
@@ -118,10 +118,13 @@ var (
 // New returns a memcache client using the provided server(s)
 // with equal weight. If a server is listed multiple times,
 // it gets a proportional amount of weight.
-func New(server ...string) *Client {
+func New(server ...string) (*Client, error) {
 	ss := new(ServerList)
-	ss.SetServers(server...)
-	return NewFromSelector(ss)
+    err := ss.SetServers(server...)
+    if err != nil {
+        return nil, err
+    }
+	return NewFromSelector(ss), nil
 }
 
 // NewFromSelector returns a new Client using the provided ServerSelector.
